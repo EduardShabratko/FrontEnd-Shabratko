98b9b2997c6ec3b49948c20997824a53d019506b
Eduard <shabratkoed@gmail.com>
2023-02-28 21:04:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('Hello');
+
+const name = prompt('What is your name?');
+
+const year = prompt('What is your birth year?');
+
+const age = 2023 - year;
+
+const country = prompt('Where are you from?');
+
+alert('Nice to meet you '  + name +   ' You are '  + age +   ' years old! ' + ' You are from ' + country);
